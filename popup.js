56fc4a0e85ba5d09d03172679ef8b55543bc3151
Neno <nebojsa.kovacevik@gmail.com>
2024-12-29 16:27:24 +0100

    refactor popup.js, add selection functionality

--- popup.js
+++ popup.js
@@ -1,48 +1,129 @@
 document.addEventListener("DOMContentLoaded", () => {
   const tabsContainer = document.getElementById("tabs");
+  const selectedTabs = [];
 
   chrome.runtime.sendMessage({ action: "getYouTubeTabs" }, (response) => {
     const tabs = response.tabs;
     if (!tabs || tabs.length === 0) {
       return;
     }
+
     tabs.forEach((tab) => {
       const tabElement = document.createElement("div");
       tabElement.classList.add("tab");
+      tabElement.setAttribute("data-id", tab.id);
 
-      tabElement.innerHTML = `
-        <div>${tab.title}</div>
-        <div class="controls">
-            <button data-id="${tab.id}" class="mute">Mute</button>
-            <button data-id="${tab.id}"
-            class="playpause">Play/Pause</button>
-            <input type="range" data-id="${tab.id}" class="volume" min="0" max="1" step="0.1">
-        </div>
-      `;
-      tabsContainer.appendChild(tabElement);
-    });
+      // Create the title element
+      const titleElement = createElement("h4", ["title"], {}, tab.title);
+      tabElement.appendChild(titleElement);
 
-    tabsContainer.addEventListener("click", (e) => {
-      button = e.target;
-      const tabId = Number(button.dataset.id);
+      // Create the controls container
+      const controlsContainer = createElement("div", ["controls"]);
 
-      if (button.classList.contains("mute")) {
-        chrome.tabs.sendMessage(tabId, { action: "toggleMute" });
-      } else if (button.classList.contains("playpause")) {
-        chrome.tabs.sendMessage(tabId, { action: "togglePlayPause" });
-      }
-    });
+      // Create the checkbox input
+      const checkbox = createElement("input", ["select"], {
+        type: "checkbox",
+        "data-id": tab.id,
+      });
+      controlsContainer.appendChild(checkbox);
 
-    tabsContainer.addEventListener("input", (e) => {
-      const slider = e.target;
-      const tabId = Number(slider.dataset.id);
+      // Create the mute button
+      const muteButton = createElement(
+        "button",
+        ["mute"],
+        { "data-id": tab.id },
+        "Mute"
+      );
+      controlsContainer.appendChild(muteButton);
 
-      if (slider.classList.contains("volume")) {
-        chrome.tabs.sendMessage(tabId, {
-          action: "setVolume",
-          volume: slider.value,
+      // Create the play/pause button
+      const playPauseButton = createElement(
+        "button",
+        ["playpause"],
+        { "data-id": tab.id },
+        "Play/Pause"
+      );
+      controlsContainer.appendChild(playPauseButton);
+
+      // Create the volume input range
+      const volumeSlider = createElement("input", ["volume"], {
+        type: "range",
+        "data-id": tab.id,
+        min: 0,
+        max: 1,
+        step: 0.05,
+        value: 0.5,
+      });
+      controlsContainer.appendChild(volumeSlider);
+
+      // SELECT CHECKBOX:
+      checkbox.addEventListener("change", () => {
+        if (checkbox.checked) {
+          // If there are already 2 selected tabs, remove the last one
+          if (selectedTabs.length >= 2) {
+            const removedTab = selectedTabs.pop();
+            removedTab.querySelector('input[type="checkbox"]').checked = false;
+            removedTab.removeAttribute("data-selected");
+          }
+
+          // Add the current tab to the selectedTabs array
+          selectedTabs.push(tabElement);
+        } else {
+          // If unchecked, remove the tab from selectedTabs
+          tabElement.removeAttribute("data-selected");
+          const index = selectedTabs.indexOf(tabElement);
+          if (index !== -1) {
+            selectedTabs.splice(index, 1);
+          }
+        }
+
+        // Update the data-selected attribute on all selected tabs
+        selectedTabs.forEach((tab, index) => {
+          tab.setAttribute("data-selected", index);
         });
-      }
+      });
+
+      muteButton.addEventListener("click", () => {
+        chrome.tabs.sendMessage(tab.id, { action: "toggleMute" });
+        if (muteButton.checked) {
+        }
+      });
+
+      playPauseButton.addEventListener("click", () => {
+        chrome.tabs.sendMessage(tab.id, { action: "togglePlayPause" });
+      });
+
+      volumeSlider.addEventListener("input", () => {
+        chrome.tabs.sendMessage(tab.id, {
+          action: "setVolume",
+          volume: volumeSlider.value,
+        });
+      });
+
+      // Append the controls container to the tab element
+      tabElement.appendChild(controlsContainer);
+
+      // Now append tabElement to the tabsContainer
+      tabsContainer.appendChild(tabElement);
     });
   });
 });
+
+function createElement(
+  tag,
+  classNames = [],
+  attributes = {},
+  textContent = ""
+) {
+  const element = document.createElement(tag);
+  if (classNames.length) {
+    element.classList.add(...classNames);
+  }
+  Object.entries(attributes).forEach(([key, value]) => {
+    element.setAttribute(key, value);
+  });
+  if (textContent) {
+    element.textContent = textContent;
+  }
+  return element;
+}
