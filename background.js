27058467ed7aa243f03c11811e8532e7ba64889c
Neno <nebojsa.kovacevik@gmail.com>
2024-12-30 02:05:22 +0100

    update background.js to get only youtube video pages

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "getYouTubeTabs") {
-    chrome.tabs.query({ url: "*://www.youtube.com/*" }, (tabs) => {
+    chrome.tabs.query({ url: "*://www.youtube.com/watch?*" }, (tabs) => {
       sendResponse({ tabs });
     });
     return true;
